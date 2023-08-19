ee0ebc9208d80571a868710004f6ac8e8bbf201b
Vitali Voinski <vitalivoi@protonmail.com>
2023-08-19 03:35:54 +0200

    Update windows-setup.bat

--- windows-setup.bat
+++ windows-setup.bat
@@ -48,7 +48,7 @@ reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Ad
 
 echo Set Wallpaper
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "C:\Windows\Web\Wallpaper\Windows\img0.jpg" /f
-reg add "HKLM\Software\Policies\Microsoft\Windows\Personalization" /v LockScreenImage /t REG_SZ /d "C:\Windows\Web\Wallpaper\Windows\img0.jpg" /f
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v RotatingLockScreenOverlayEnabled /t REG_DWORD /d 0 /f
 
 echo Clear Desktop Downloads
 del /q "%userprofile%"\Desktop\*
