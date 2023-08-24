b55bcad4a1788fbe4ebe83400d297e5e29584783
Vitali Voinski <vitalivoi@protonmail.com>
2023-08-24 16:56:03 +0200

    Update windows-setup.bat

--- windows-setup.bat
+++ windows-setup.bat
@@ -13,6 +13,7 @@ echo Add Parsec
 cd "%userprofile%"\Downloads"
 curl https://builds.parsecgaming.com/package/parsec-windows.exe -o parsec-windows.exe
 parsec-windows.exe /silent /shared /vdd
+timeout 5
 del /f parsec-windows.exe
 
 echo Remove Apps
