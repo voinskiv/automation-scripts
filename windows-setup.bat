7f5422ed3edb3d865a23e84752a6f151a5051a68
Vitali Voinski <v.voinski@icloud.com>
2024-04-06 23:41:00 +0200

    Update windows-setup.bat

--- windows-setup.bat
+++ windows-setup.bat
@@ -47,7 +47,6 @@ echo Add Microsoft Teams
 cd "%UserProfile%"\Downloads"
 curl -L https://go.microsoft.com/fwlink/?linkid=2243204 -o teamsbootstrapper.exe
 teamsbootstrapper.exe -p
-pause
 
 echo Add OS Updates
 usoclient ScanInstallWait
