724da078d1882fba83300dc7ac726d0d567d63ca
Vitali Voinski <v.voinski@icloud.com>
2024-04-15 09:47:46 +0200

    Update macos-setup.sh

--- macos-setup.sh
+++ macos-setup.sh
@@ -1,1 +1,15 @@
 #! /bin/bash
+
+# Remove Apps
+
+# Add OS Updates
+
+# Add App Updates
+
+# Activate Tap to Click
+
+# Deactivate Recent Apps in Dock
+
+# Remove Safari Favorites
+
+# Remove Signature on iOS
