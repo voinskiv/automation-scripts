2d0218a051755ab31cae81eca7b29216f09bb56f
Vitali Voinski <v.voinski@icloud.com>
2024-04-21 23:06:30 +0200

    Update backup.bat

--- backup.bat
+++ backup.bat
@@ -1,7 +1,17 @@
 @echo off
 
-set destination=--onedrive-no-versions onedrive:"\Backups\%COMPUTERNAME%
+set destination=onedrive:\Backups\%COMPUTERNAME%
+set timestamp=%date:~6,4%-%date:~3,2%-%date:~0,2%-%time:~0,2%%time:~3,2%%time:~6,2%
+set options=--progress --onedrive-no-versions
+set filter=--exclude .*/ --exclude {~*,*.ini}
+set log=--log-file "%APPDATA%\rclone\backup.log"
+set settings=%options% %filter% %log%
 
-rclone sync "%USERPROFILE%\Desktop" %destination%\Desktop\Letztes" --backup-dir %destination%\Desktop\%date%"
-rclone sync "%USERPROFILE%\Documents" %destination%\Dokumente\Letztes" --backup-dir %destination%\Dokumente\%date%"
-rclone sync "G:\Meine Ablage" %destination%\Meine Ablage\Letztes" --backup-dir %destination%\Meine Ablage\%date%"
+echo Back up Desktop files
+rclone sync %settings% "%USERPROFILE%\Desktop" %destination%\Desktop\Letztes --backup-dir %destination%\Desktop\%timestamp% 
+
+echo Back up Documents files
+rclone sync %settings% "%USERPROFILE%\Documents" %destination%\Dokumente\Letztes --backup-dir %destination%\Dokumente\%timestamp%
+
+echo Back up Google Drive files
+rclone sync %settings% "G:\Meine Ablage" "%destination%\Meine Ablage\Letztes" --backup-dir "%destination%\Meine Ablage\%timestamp%"
