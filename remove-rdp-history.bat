b193fb34f8f213c2a4d5fde4e06616f7433a4b2b
Vitali Voinski <v.voinski@icloud.com>
2024-04-27 04:59:34 +0200

    Update remove-rdp-history.bat

--- remove-rdp-history.bat
+++ remove-rdp-history.bat
@@ -1,4 +1,5 @@
 @echo off
+
 reg delete "HKEY_CURRENT_USER\Software\Microsoft\Terminal Server Client\Default" /va /f
 reg delete "HKEY_CURRENT_USER\Software\Microsoft\Terminal Server Client\Servers" /f
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Terminal Server Client\Servers"
