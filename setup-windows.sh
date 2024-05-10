0d870ea7a774bfb4e2e6eaf2eb8a3a084a7f5b03
Vitali Voinski <18062353+voinskiv@users.noreply.github.com>
2024-05-10 11:26:25 +0200

    Update setup-windows.sh

--- setup-windows.sh
+++ setup-windows.sh
@@ -2,7 +2,10 @@
 #
 # winget install --name "Git" --source winget
 #
-# Install WinGet packages, optimise and set settings.
+# Install WinGet packages, optimise and set settings
+
+# Path to Dell Command Update CLI
+dcu_cli="/C/Program Files/Dell/CommandUpdate/dcu-cli.exe"
 
 apps_to_uninstall=(
   "Tailscale"
@@ -24,6 +27,13 @@ dell_apps_to_uninstall=(
   "Dell SupportAssist Remediation"
 )
 
+uninstall_apps() {
+  local app_list=("$@")
+  for app in "${app_list[@]}"; do
+    winget uninstall --name "$app" --accept-source-agreements --silent
+  done
+}
+
 apps_to_install=(
   "Clipchamp"
   "Family"
@@ -39,41 +49,40 @@ apps_to_install=(
   "Xbox TCUI"
 )
 
-registry_settings=(
-    # Set desktop background image
-    "HKCU\Control Panel\Desktop Wallpaper C:\Windows\Web\Wallpaper\Windows\img0.jpg"
-    # Change lock screen
-    "HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager RotatingLockScreenEnabled 0"
-    "HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager RotatingLockScreenOverlayEnabled 0"
-    # Turn off search on taskbar
-    "HKCU\Software\Microsoft\Windows\CurrentVersion\Search SearchboxTaskbarMode 0"
-    # Turn off chat on taskbar
-    "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced TaskbarMn 0"
-    # Turn off widgets on taskbar
-    "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced TaskbarDa 0"
-    # Turn off Bing in search
-    "HKCU\Software\Microsoft\Windows\CurrentVersion\Search BingSearchEnabled 0"
-)
-
-uninstall_apps() {
-  local app_list=("$@")
-  for app in "${app_list[@]}"; do
-    winget uninstall --name "$app" --accept-source-agreements --silent
-  done
-}
-
 install_apps() {
-  local app_list=("$@")
-  for app in "${app_list[@]}"; do
+  for app in "${apps_to_install[@]}"; do
     winget install --name "$app" --accept-source-agreements --silent
   done
 }
 
+registry_settings=(
+  # Set desktop background image
+  "HKCU\Control Panel\Desktop Wallpaper C:\Windows\Web\Wallpaper\Windows\img0.jpg"
+  # Change lock screen
+  "HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager RotatingLockScreenEnabled 0"
+  "HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager RotatingLockScreenOverlayEnabled 0"
+  # Turn off search on taskbar
+  "HKCU\Software\Microsoft\Windows\CurrentVersion\Search SearchboxTaskbarMode 0"
+  # Turn off chat on taskbar
+  "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced TaskbarMn 0"
+  # Turn off widgets on taskbar
+  "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced TaskbarDa 0"
+  # Turn off Bing in search
+  "HKCU\Software\Microsoft\Windows\CurrentVersion\Search BingSearchEnabled 0"
+  "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" "fDenyTSConnections" "0"
+)
+
+apply_registry_settings() {
+  for setting in "${registry_settings[@]}"; do
+      set_registry_value "$setting"
+  done
+}
+
 set_registry_value() {
-    local reg_key="$1"
-    local value_name="$2"
-    local value_data="$3"
-    reg add "$reg_key" /v "$value_name" /t REG_SZ /d "$value_data" /f
+  local reg_key="$1"
+  local value_name="$2"
+  local value_data="$3"
+  reg add "$reg_key" /v "$value_name" /t REG_SZ /d "$value_data" /f
 }
 
 uninstall_apps "${apps_to_uninstall[@]}"
@@ -82,100 +91,34 @@ if [[ "$(wmic csproduct get vendor | grep -o 'Dell')" == "Dell" ]]; then
   uninstall_apps "${dell_apps_to_uninstall[@]}"
 fi
 
-install_apps "${apps_to_install[@]}"
+install_apps
 
-# Iterate over each registry setting and apply it
-for setting in "${registry_settings[@]}"; do
-    set_registry_value $setting
-done
+apply_registry_settings
 
+del /q "$USERPROFILE/Desktop/*"
 
+# Check for and initiate OS updates
+usoclient StartInteractiveScan
 
+# Configure Dell automatic scheduling and apply updates
+if [ -f "$dcu_cli" ]; then
+  "$dcu_cli" //configure -scheduleAuto
+  "$dcu_cli" //applyUpdates
+fi
 
-
-
-
-
-
-
-
-echo "
-alias update=\"usoclient StartInteractiveScan\"
-alias dellupdate=\"usoclient StartInteractiveScan\" " >> ~/.bash_profile; . ~/.bash_profile
-
-# Set up data encryption.
+# Activate BitLocker encryption and retrieve recovery key
 manage-bde -on C:
-echo -e ${YELLOW}Save recovery key to turn on data encryption${ENDCOLOR}
+manage-bde -protectors -get C: -type recoverypassword
 
-# Update system.
-## Deploy Windows updates.
-update
-## Deploy Dell updates.
-"/C/Program Files/Dell/CommandUpdate/dcu-cli.exe" //configure -scheduleAuto
-"/C/Program Files/Dell/CommandUpdate/dcu-cli.exe" //applyUpdates
-
-# Set system settings.
-## Change desktop background image.
-reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "C:\Windows\Web\Wallpaper\Windows\img0.jpg" /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\DesktopSpotlight\Settings" /v EnabledState /t REG_DWORD /d "0" /f
-## Change lock screen.
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v RotatingLockScreenEnabled /t REG_DWORD /d 0 /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v RotatingLockScreenOverlayEnabled /t REG_DWORD /d 0 /f
-## Turn off search on taskbar.
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Search" /v SearchboxTaskbarMode /t REG_DWORD /d 0 /f
-## Turn off chat on taskbar.
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarMn /t REG_DWORD /d 0 /f
-## Turn off widgets on taskbar.
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarDa /t REG_DWORD /d 0 /f
-## Turn off Bing in search.
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Search" /v BingSearchEnabled /t REG_DWORD /d 0 /f
-
-
-
-reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "C:\Windows\Web\Wallpaper\Windows\img0.jpg" /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\DesktopSpotlight\Settings" /v EnabledState /t REG_DWORD /d "0" /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v RotatingLockScreenEnabled /t REG_DWORD /d 0 /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v RotatingLockScreenOverlayEnabled /t REG_DWORD /d 0 /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Search" /v SearchboxTaskbarMode /t REG_DWORD /d 0 /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarMn /t REG_DWORD /d 0 /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarDa /t REG_DWORD /d 0 /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Search" /V BingSearchEnabled /T REG_DWORD /D 0 /f
-
-
-
-
-
-
-
-
-
-# Turn on remote login using SSH.
+# Install SSH Server and set to start automatically
 dism /Online /Add-Capability /CapabilityName:OpenSSH.Server
-powershell -Command "& {Set-Service -Name sshd -StartupType 'Automatic';}"
-powershell -Command "& {Start-Service sshd;}"
+sc config ssh start=auto
+sc start sshd
 
-# Turn on remote management using Remote Desktop.
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
-netsh advfirewall firewall set rule group="Remotedesktop" new enable=Yes
+# Turn on Remote Desktop
+set_registry_value "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" "fDenyTSConnections" "0"
+netsh advfirewall firewall set rule group="remote desktop" new enable=Yes
 
-# Disable not needed startup apps.
-reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "SecurityHealth" /f
-reg delete "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "OneDrive" /f
-
-# Clear Desktop and Downloads.
-del /q "%UserProfile%\Desktop\*"
-del /q "%Public%\Desktop\*"
-del /q "%UserProfile%\Downloads\*"
-rd /s /q "%SystemDrive%\$recycle.bin"
-
-
-
-
-YELLOW="\e[33m"; ENDCOLOR="\e[0m"
-# Action required.
-echo [ ] Create PIN to sign in
-echo [ ] Clear taskbar and start menu
-echo [ ] Update pre-installed apps
-echo [ ] Set up installed apps
-
-pause
+# Disable startup items
+wmic startup where name="SecurityHealth" call disable
+wmic startup where name="OneDrive" call disable
